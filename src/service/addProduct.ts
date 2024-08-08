d3878543359f6dc97106167455f4dfccdf421814
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-08 11:17:08 +0530

    Add: adding the product

---
+++ src/service/addProduct.ts
@@ -0,0 +1,20 @@
+import {Product} from '../model/product';
+
+let products = new Array<Product>();
+
+let product1 = new Product(1, 'Apple', 100, 'Fruit');
+let product2 = new Product(2, 'Earbuds', 200, 'Electronics');
+let product3 = new Product(3, 'Laptop', 300, 'Electronics');
+let product4 = new Product(4, 'Mango', 400, 'Fruit');
+let product5 = new Product(5, 'Shirt', 500, 'Clothing');
+let product6 = new Product(6, 'Trouser', 600, 'Clothing');
+let product7 = new Product(7, 'Orange', 700, 'Fruit');
+let product8 = new Product(8, 'Headphones', 800, 'Electronics');
+let product9 = new Product(9, 'T-shirt', 900, 'Clothing');
+let product10 = new Product(10, 'Jeans', 1000, 'Clothing');
+
+
+products.push(product1);
+products.push(product2);
+products.push(product3);
+products.push(product4);
